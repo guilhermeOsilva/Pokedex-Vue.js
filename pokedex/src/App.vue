ec4e08bdc020f1b9e32ccf520c0f5ec657d53211
guilhermeOsilva <guilherme.osilva@bandtec.com.br>
2022-10-21 18:24:56 -0300

    adicionando o mecanismo de busca de pokemons

--- pokedex/src/App.vue
+++ pokedex/src/App.vue
@@ -1,11 +1,12 @@
 <template>
   <div id="app">
     <div class="column is-half is-offset-one-quarter">
-      <img src="./assets/8614.jpg" alt="">
+      <img src="./assets/8614.jpg" alt="" id="imagePokemons">
       <hr>
       <h4 class="is-size-4">Pokedex</h4>
-
-      <div v-for="(poke, index) in pokemons" :key="index">
+      <input type="text" class="input is-rounded" placeholder="Buscar pokemon pelo nome" v-model="busca">
+      <button class="button is-info is-outlined is-fullwidth" id="buscaBtn" @click="buscar">Buscar</button>
+      <div v-for="(poke, index) in filteredPokemons" :key="poke.url">
         <Pokemon :name="poke.name" :url="poke.url" :num="index + 1" />
       </div>
     </div>
@@ -22,6 +23,9 @@ export default {
   data() {
     return {
       pokemons: [],
+      filteredPokemons: [],
+      busca: '',
+
     };
   },
   created: function () {
@@ -29,12 +33,22 @@ export default {
       .get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
       .then((response) => {
         this.pokemons = response.data.results;
+        this.filteredPokemons = response.data.results;
         console.log(this.pokemons);
       });
   },
   components: {
     Pokemon
-  }
+  },
+  methods: {
+    buscar: function () {
+      if (this.busca == '' || this.busca == ' ') {
+        this.filteredPokemons = this.pokemons;
+      } else {
+        this.filteredPokemons = this.pokemons.filter(pokemon => pokemon.name == this.busca);
+      }
+    }
+  },
 };
 </script>
 
@@ -46,5 +60,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+
+
+#buscaBtn {
+  margin-top: 2%;
 }
 </style>
